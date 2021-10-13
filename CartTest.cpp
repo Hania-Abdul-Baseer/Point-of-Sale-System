0cb3bb9fbe2ee1cb61e11d3c9de7fd057f53322a
Hania <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2021-10-13 10:36:27 +1030

    Create CartTest.cpp

---
+++ CartTest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Payment.h"
+#include "Product.h"
+#include "Cart.h"
+#include "Receipt.h"
+using namespace std;
+
+int main(){
+	cout<<"This is Cart test"<<endl;
+	return 0;
+}
