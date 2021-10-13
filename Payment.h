897a2958a9d6b4f1e17771c597272f13775223b8
Hania <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2021-10-13 10:33:32 +1030

    Update Payment.h

--- Payment.h
+++ Payment.h
@@ -1,11 +1,6 @@
 #ifndef PAYMENT_H
 #define PAYMENT_H
-#ifndef CARD_H
-#define CARD_H
-#ifndef LATERPAY_H
-#define LATERPAY_H
-#ifndef CASH_H
-#define CASH_H
+
 #include <iostream>
 #include <string>
 #include "Cart.h"
@@ -18,52 +13,56 @@ public:
 	string user_name; 
 
 	Payment();
-	double getTotalPrice(Cart *order_cart); //gets the sum price from the card array
+	double getPrice(Cart *order_cart); //gets the sum price from the card array
 	virtual void setPaymentInfo(); //gets the customer's name 
 	virtual void getPaymentInfo(); //allows the sub classes to define this fucntion
-	~Payment();
+    ~Payment();
 };
-#endif
+
 
 //Subclass of Payment: Card
 class Card: public Payment{
-private:
+//private:
+	
+
+public:
 	int card_num[16];  //card number is 16 digit long
 	string exp_date;   //card expiration date
 	int cvv;
 	string card_name;  //card holder's name
 					   //the name as displayed on the card
-	
-	virtual void setPaymentInfo();
 
-public:
 	Card();
 	int card_end_num[4];  //the last 4 digits of the card number 
 	virtual void getPaymentInfo();
+	virtual void setPaymentInfo();
 	~Card();
 };
-#endif
+
 
 //Subclass of Payment: Cash
 class Cash: public Payment{
-private:
-	double change;
-	double amount_paid;
-	virtual void setPaymentInfo();
+//private:
+	
 
 public:
+	double change;
+	double amount_paid;
 	Cash();
 	void setAmountPaid();
 	double getAmountPaid();
 	double getChange();
+	virtual void setPaymentInfo();
 	virtual void getPaymentInfo();
 	~Cash();
 };
-#endif
+
 
 //Subclass of Payment: LaterPay
 class LaterPay: public Payment{
-private:
+//private:
+	
+public:
 	int card_num[16];  //card number is 16 digits long
 	int installments_num;
 	double price_per_installment;
@@ -71,12 +70,11 @@ private:
 	int cvv;
 	string card_name;  //card holder's name
 					   //the name as displayed on the card
-	virtual void setPaymentInfo();
-
-public:
+	
 	int card_end_num[4];  //last 4 digits of card for customer's 
 						  //reference printed in receipt
 	LaterPay();
+	virtual void setPaymentInfo();
 	virtual void getPaymentInfo();
 	int getInstallmentsNum();
 	double getPricePerInstallment();
