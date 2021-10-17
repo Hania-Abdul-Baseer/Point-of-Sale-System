2290c5f9435c30e45b0920cb72b50b3b090ad41f
CathHuo378 <83620582+CathHuo378@users.noreply.github.com>
2021-10-18 01:22:49 +1030

    Update Cart.cpp

--- Cart.cpp
+++ Cart.cpp
@@ -19,10 +19,10 @@ Cart::Cart(int inventory_size){
 void Cart::addProduct(Product item){
 
     // this variable is equal to the amount of items in the array
-    //this ensures that the for-loop will end even if cart_count is added to 
+    //this ensures that the for-loop will end even if cart_count is added to
     int index = cart_count;
 
-    // check if the quantity reqruied/added for this product is 0 or less 
+    // check if the quantity reqruied/added for this product is 0 or less
     if (!(item.getQuantityRequired() > 0)){
         // if it is then this function will end
         return;
@@ -34,12 +34,12 @@ void Cart::addProduct(Product item){
     }
     else{
         int count = 0; // will count if the same product is already in the cart_list
-        
+
         // if loop to check if the item is in the cart array
         for (int i = 0; i < index; ++i){
-            if (cart_list[i].getProductCode() == item.getProductCode()){   
+            if (cart_list[i].getProductCode() == item.getProductCode()){
                 // if the product is in the cart list it will be updated with the new item
-                cart_list[i] = item; 
+                cart_list[i] = item;
                 count = count +1; // will be added to
             }
         }
@@ -49,7 +49,7 @@ void Cart::addProduct(Product item){
             cart_list[cart_count++] = item;
         }
     }
-    
+
 }
 
 // get the total price of products in the cart
@@ -57,12 +57,17 @@ double Cart::getTotalPrice(){
     //Will ensure that total price doesnt accumulate as the function is called
     total_price = 0;
     // will loop through the cart list and get the product price
-    for (int i = 0; i < cart_count; ++i){   
+    for (int i = 0; i < cart_count; ++i){
         // product price will be added to the total price
         total_price = total_price + cart_list[i].getProductPrice();
     }
-    
+
     return total_price;// total price for products in cart  is returned
+}
+
+// get the list of products
+Product* Cart::getCartList(){
+    return cart_list;
 }
 
 //destructor
