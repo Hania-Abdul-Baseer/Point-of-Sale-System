6178d95c322a2e8bb1b6690e88affecca096d49f
CathHuo378 <83620582+CathHuo378@users.noreply.github.com>
2021-10-18 01:24:30 +1030

    Update ReceiptTest.cpp

--- ReceiptTest.cpp
+++ ReceiptTest.cpp
@@ -1,11 +1,50 @@
-#include <iostream>
-#include "Payment.h"
-#include "Product.h"
-#include "Cart.h"
+//a test file for the class Receipt
 #include "Receipt.h"
-using namespace std;
+
 
 int main(){
-	cout<<"The Receipt class compiled successfully"<<endl;
-	return 0;
+
+    //initial values
+    Product* cart = new Product[5];
+    int count = 5;
+    double total_price = 10.05;
+    Payment* method = new Card;
+
+    //initialise two Receipt variables
+    //Receipt R1;
+    ReceiptByEmail R1;
+    ReceiptByMessage R2;
+    Receipt R3;
+
+    //testing functions
+    //print list of product
+    R2.displayReceipt(cart, count, total_price, method);
+
+
+    //get details and check valid input
+
+    cout << R1.checkValidInput(R1.getDetails()) << endl;
+    cout << R1.checkValidInput(R1.getDetails()) << endl;
+    cout << R1.checkValidInput(R1.getDetails()) << endl;
+    cout << R1.checkValidInput(R1.getDetails()) << endl;
+    cout << R1.checkValidInput(R1.getDetails()) << endl;
+    cout << R1.checkValidInput(R1.getDetails()) << endl;
+    cout << R2.checkValidInput(R2.getDetails()) << endl;
+    cout << R2.checkValidInput(R2.getDetails()) << endl;
+    cout << R2.checkValidInput(R2.getDetails()) << endl;
+    cout << R2.checkValidInput(R2.getDetails()) << endl;
+    cout << R2.checkValidInput(R2.getDetails()) << endl;
+
+    //notification
+    R1.notify();
+    R2.notify();
+
+
+    //destructor
+    //R1.~Receipt();
+    R1.~Receipt();
+    R2.~Receipt();
+
+
+    return 0;
 }
