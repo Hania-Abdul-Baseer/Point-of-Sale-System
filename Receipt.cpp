4154627d04091c9d0fc883f532666ffc6d19046f
CathHuo378 <83620582+CathHuo378@users.noreply.github.com>
2021-10-18 01:23:57 +1030

    Update Receipt.cpp

--- Receipt.cpp
+++ Receipt.cpp
@@ -1,52 +1,60 @@
-
 #include "Receipt.h"
 
 //basic class Receipt
 //implementation of the default Receipt constructor
 Receipt::Receipt(){
-    Product* cart_list = nullptr;
-    double total_price = 0;
-    Payment method = " ";
-    char* date_time = nullptr;
-}
-//Receipt with the list of product, total price and payment method
-Receipt::Receipt(Product* plist, double ptotal_price, Payment pmethod){
-    Product* cart_list = plist;
-    double total_price = ptotal_price;
-    Payment method = pmethod;
-    char* date_time = nullptr;
+
 }
 
-//get the list of product
-Product* Receipt::getCart(){
-    return cart_list;
-}
 
-//get the total price
-double Receipt::getTotalPrice(){
-    return total_price;
-}
+//function to display receipt
+void Receipt::displayReceipt(Product* plist, int pitem_count, double ptotal_price, Payment* pmethod){
+    //display Receipt header
+    cout << "\n\n\t\t\tBest Buy Grocery Store\n\t\t\t\t 73 Rundle Mall\n\t\t\t   Adelaide SA 5000\n\t\t\twww.bestbuygrocery.com\n\t\t\tTelephone: 08 8000 8000\n\t\t\t ABN: 12 345 678 900\n\t\t\t      TAX INVOICE\n" << endl;
 
-//get Payment Method
-Payment Receipt::getPaymentMethod(){
-    return method;
-}
-
-//get current time
-char* Receipt::getDateTime(){
+    //get current date time
     //current date and time on the current system
     time_t now = time(0);
-
     //convert now to string form
     char* date_time = ctime(&now);
+    //print date time
+    cout << "Date/Time -- " << date_time << endl;
 
-    return date_time;
+    //product info
+    int i = 0;
+    while (i != pitem_count){
+        if (plist[i].getQuantityRequired() != 0){
+            cout << i+1 << ". " << plist[i].getProductName() << "\t\t\t\t\t" << plist[i].getProductPrice() << endl;
+        }
+        i++;
+    }
+
+    //total price
+    cout << "TOTAL AUD" << "\t     " << ptotal_price << "\n" << endl;
+
+    //payment info
+    pmethod->getPaymentInfo();
+
+    //extra info
+    cout << "\nExchange and refund will not be available.\nThank you for shopping at Good Buy Grocery Store." << endl;
+
+}
+
+string Receipt::getDetails(){
+    return " ";
+}
+
+bool Receipt::checkValidInput(string input){
+    return " ";
+}
+
+void Receipt::notify(){
+
 }
 
 //destructor to free heap memory
 Receipt::~Receipt(){
-    delete[] cart_list;
-    delete date_time;
+
 }
 
 //subclass ReceiptByEmail
@@ -55,13 +63,6 @@ ReceiptByEmail::ReceiptByEmail(){
     string email = " ";
 }
 
-//ReceiptByEmail with the list of product, total price and payment method
-ReceiptByEmail::ReceiptByEmail(Product* plist, double ptotal_price, Payment pmethod){
-    Product* cart_list = plist;
-    double total_price = ptotal_price;
-    Payment method = pmethod;
-    char* date_time = nullptr;
-}
 
 //get email from the user
 string ReceiptByEmail::getDetails(){
@@ -75,6 +76,38 @@ void ReceiptByEmail::notify(){
     cout << "The receipt has been successfully sent to " << email << endl;
 }
 
+bool ReceiptByEmail::checkValidInput(string email){
+
+    int length = email.length();
+    for(int j = 1; j < length; j++){
+        if (!((email[j] >= 'a' && email[j] <= 'z') || (email[j] >= 'A' && email[j] <= 'Z') || (email[j] >= '0' && email[j] <= '9') || (email[j] == '@') || (email[j] == '.'))){
+            return false;
+        }
+    }
+
+    int at_position = -1;
+    int at_count = 0;
+    int dot_position = -1;
+
+    for (int i = 0; i < length; i++){
+        if (email[i] == '@'){
+            at_position = i;
+            at_count++;
+        }
+        else if (email[i] == '.'){
+            dot_position = i;
+            break;
+        }
+    }
+
+
+    if ((at_count != 1) || (at_position == -1) || (dot_position == -1) || (at_position > dot_position) || (dot_position == (length-1))){
+        return false;
+    }
+
+    return true;
+}
+
 ReceiptByEmail::~ReceiptByEmail(){
 
 }
@@ -85,13 +118,6 @@ ReceiptByMessage::ReceiptByMessage(){
     string phone_number = " ";
 }
 
-//ReceiptByMessage with the list of product, total price and payment method
-ReceiptByMessage::ReceiptByMessage(Product* plist, double ptotal_price, Payment pmethod){
-    Product* cart_list = plist;
-    double total_price = ptotal_price;
-    Payment method = pmethod;
-    char* date_time = nullptr;
-}
 
 //get phone number from the user
 string ReceiptByMessage::getDetails(){
@@ -103,6 +129,21 @@ string ReceiptByMessage::getDetails(){
 //notification for sent receipt
 void ReceiptByMessage::notify(){
     cout << "The receipt has been successfully sent to " << phone_number << endl;
+}
+
+bool ReceiptByMessage::checkValidInput(string phone_number){
+    int length = phone_number.length();
+    if (length != 10){
+        return false;
+    }
+    else{
+        for (int i = 0; i < 10; i++){
+            if (isdigit(phone_number[i]) == false){
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 ReceiptByMessage::~ReceiptByMessage(){
