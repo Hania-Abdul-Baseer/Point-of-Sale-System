d0dbe93699a67ad9f4bb140510709f2a4f8c55f3
Hania <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2021-10-18 15:40:40 +1030

    Update Payment.h

--- Payment.h
+++ Payment.h
@@ -27,14 +27,14 @@ class Card: public Payment{
 //private:
 	
 public:
-	int card_num[16];  //card number is 16 digit long
+	string card_num;  //card number is 16 digit long
 	string exp_date;   //card expiration date
-	int cvv;
+	string cvv;
 	string card_name;  //card holder's name
 					   //the name as displayed on the card
 
 	Card();
-	int card_end_num[4];  //the last 4 digits of the card number 
+	char card_end_num[4];  //the last 4 digits of the card number 
 	virtual void getPaymentInfo();
 	virtual void setPaymentInfo();
 	~Card();
@@ -64,15 +64,15 @@ class LaterPay: public Payment{
 //private:
 	
 public:
-	int card_num[16];  //card number is 16 digits long
+	string card_num;  //card number is 16 digits long
 	int installments_num;
 	double price_per_installment;
 	string exp_date;   // card expiration date
-	int cvv;
+	string cvv;
 	string card_name;  //card holder's name
 					   //the name as displayed on the card
 	
-	int card_end_num[4];  //last 4 digits of card for customer's 
+	char card_end_num[4];  //last 4 digits of card for customer's 
 						  //reference printed in receipt
 	LaterPay();
 	virtual void setPaymentInfo();
@@ -82,8 +82,6 @@ public:
 	~LaterPay();
 };
 #endif
-
-
 
 
 
