011feedea2b7de08e6cc573a8305eea5e2343671
Hania <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2021-10-19 01:50:23 +1030

    Update PaymentTest.cpp

--- PaymentTest.cpp
+++ PaymentTest.cpp
@@ -5,57 +5,51 @@
 using namespace std;
 
 int main(){
-
-	cout<<"----------------------------Card unit test------------------------"<<endl;
-
+	cout<<"------------------------------Card unit test-----------------------------"<<endl;
 	Card c1 = Card();
 	c1.setPaymentInfo();
-
-	cout<<"Expected card holder name: Hania Abdul Baseer. "<<"Actual card holder name: ";
-	cout<<c1.card_name<<"\n\n";
-	cout<<"Expected cvv number: 123. "<<"Actual cvv number: ";
-	cout<<c1.cvv<<"\n\n";
-	cout<<"Expected card expiry date: 12/12/12. "<<"Actual card expiry date: ";
-	cout<<c1.exp_date<<"\n\n";
-	cout<<"Expected ending card number: 3456. "<<"Actual ending card number: ";
-	for(int i=0;i<4;i++){
-		cout<<c1.card_end_num[i];
-	}
-	cout<<"\n\n";
-
 	c1.getPaymentInfo();
-	cout<<"\n\n";
 
-	cout<<"----------------------------Cash unit test------------------------"<<endl;
+	Card c2 = Card();
+	c2.setPaymentInfo();
+	c2.getPaymentInfo();
+
+	cout<<"-------------------------------Cash unit test------------------------------"<<endl;
 	Cash ca1 = Cash();
+	ca1.sum_price = 31.4;
 	ca1.setPaymentInfo();
-	cout<<"\n";
-	cout<<"Expected Change: -"<<ca1.getAmountPaid()<<". "<<"Actual Change: "<<ca1.getChange()<<"\n\n";
 	ca1.getPaymentInfo();
-	cout<<"\n\n";
 
-	cout<<"-------------------------LaterPay unit test--------------------------"<<endl;
+	Cash ca2 = Cash();
+	ca2.sum_price = 143.43;
+	ca2.setPaymentInfo();
+	ca2.getPaymentInfo();
+	
+	cout<<"----------------------------LaterPay unit test-----------------------------"<<endl;
+	//checking installments of 2
 	LaterPay l1 = LaterPay();
+	l1.sum_price = 325.65;
 	l1.setPaymentInfo();
-	cout<<"\n";
-
-	cout<<"Expected card holder name: Hania Abdul Baseer. "<<"Actual card holder name: ";
-	cout<<l1.card_name<<"\n\n";
-	cout<<"Expected cvv number: 123. "<<"Actual cvv number: ";
-	cout<<l1.cvv<<"\n\n";
-	cout<<"Expected card expiry date: 12/12/12. "<<"Actual card expiry date: ";
-	cout<<l1.exp_date<<"\n\n";
-	cout<<"Expected number of installments: 4. "<<"Actual number of installments: ";
-	cout<<l1.installments_num<<"\n\n";
-	cout<<"Expected ending card number: 3456. "<<"Actual ending card number: ";
-	for(int i=0;i<4;i++){
-		cout<<l1.card_end_num[i];
-	}
-	cout<<"\n\n";
-	cout<<"Expected price per installment: 0. "<<"Actual price per installment: "<<l1.getPricePerInstallment()<<"\n\n";
-
 	l1.getPaymentInfo();
-	cout<<"\n\n";
+
+	//checking installments of 3
+	LaterPay l2 = LaterPay();
+	l2.sum_price = 325.65;
+	l2.setPaymentInfo();
+	l2.getPaymentInfo();
+
+	//checking installments of 4
+	LaterPay l3 = LaterPay();
+	l3.sum_price = 325.65;
+	l3.setPaymentInfo();
+	l3.getPaymentInfo();
+
+	//checking other invalid inputs
+	LaterPay l4 = LaterPay();
+	l4.sum_price = 325.65;
+	l4.setPaymentInfo();
+	l4.getPaymentInfo();
 
 	return 0;
 }
+
