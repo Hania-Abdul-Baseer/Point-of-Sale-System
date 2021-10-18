acd761b40c6c1da2819b328d8071c88783d77b19
Hania <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2021-10-18 15:40:02 +1030

    Update Payment.cpp

--- Payment.cpp
+++ Payment.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "Payment.h"
 #include "Cart.h"
+#include "ctype.h"
 using namespace std;
 
 //deafult payment constructor 
@@ -39,36 +41,63 @@ Payment::~Payment(){
 //default Card constructor 
 Card::Card(){
 	exp_date = " ";
-	cvv = 0;
+	cvv = " ";
 	card_name = " ";
 	method_name = "Card";
+	card_num = " ";
 }
 
 //asks and stores all the info related to card payment 
 void Card::setPaymentInfo(){
+
 	cout<<"You have chosen the card payment method."<<endl;
 	cout<<"Please enter your card details to complete payment"<<endl;
 	cout<<"Cardholder Name: ";
-	cin>>card_name;
+	cin.ignore();
+    getline(cin, card_name); 
 	cout<<"\n";
+	
 	cout<<"Card Number: ";
-	for(int i=0;i<16;i++){     //stores card number in an int array of 16 digits
-		cin>>card_num[i];
-	}
+	cin.ignore();
+    getline(cin, card_num);
+    
+    while(card_num.length()!=15){
+    	cout<<"\n"<<"Invalid card number. Card number must contain 16 digits."<<endl;
+    	cout<<"Please enter the Card Number again: ";
+		cin.ignore();
+	    getline(cin, card_num);
+    }
 	cout<<"\n";
+
 	cout<<"Card Expiration Date: ";
 	cin>>exp_date;
 	cout<<"\n";
+
 	cout<<"Three Digit CVV Code: ";
-	cin>>cvv;
+	cin.ignore();
+    getline(cin, cvv);  
+    if(cvv.length()!=3){
+    	cout<<"\n"<<"Invalid cvv code. Cvv code must contain 3 digits."<<endl;
+    	cout<<"Please enter your three digit cvv code again: ";
+		cin.ignore();
+	    getline(cin, cvv);
+    	while(cvv.length()!=2){
+    		cout<<"\n"<<"Invalid cvv code. Cvv code must contain 3 digits."<<endl;
+	    	cout<<"Please enter your three digit cvv code again: ";
+			cin.ignore();
+		    getline(cin, cvv);
+       }
+    }
+    
 	cout<<"\n";
 
 //storing the last four ending digits of the card number for displaying when 
 //needed for customer's reference 
-	card_end_num[0] = card_num[12]; 
-	card_end_num[1] = card_num[13];
-	card_end_num[2] = card_num[14];
-	card_end_num[3] = card_num[15];
+	
+	card_end_num[0] = card_num[11]; 
+	card_end_num[1] = card_num[12];
+	card_end_num[2] = card_num[13];
+	card_end_num[3] = card_num[14];
 }
 
 //only displaying the payment mmethod name and ending 4 digits of card 
@@ -107,7 +136,14 @@ void Cash::setPaymentInfo(){
 	cout<<"You have chosen the cash payment option."<<endl;
 	cout<<"Enter the amount of cash paid: ";
 	cin>>amount_paid;
-	cout<<"\n";
+
+	while(amount_paid<sum_price){
+		cout<<"\n"<<"Sorry the amount paid is insufficient"<<endl;
+		cout<<"Your cart total to be paid is: $"<<sum_price<<endl;
+		cout<<"Enter the amount of cash paid: ";
+		cin>>amount_paid;
+		cout<<"\n";
+	}
 }
 
 void Cash::getPaymentInfo(){
@@ -132,45 +168,79 @@ LaterPay::LaterPay(){
 	price_per_installment = 0.0;
 	exp_date = " ";
 	card_name = " ";
-	cvv = 0;
+	cvv = " ";
 	card_name = " ";
 	method_name = "LaterPay";
 }
 
 //asks and stores all info related to LaterPay payment method 
 void LaterPay::setPaymentInfo(){
+	double installments_num_double;
 	cout<<"You have chosen the LaterPay payment option."<<endl;
 	cout<<"Please enter your card details and installment options to complete payment."<<endl;
 	cout<<"Cardholder Name: ";
-	cin>>card_name;
+	cin.ignore();
+    getline(cin, card_name);
+   
 	cout<<"\n";
 	cout<<"Card Number: ";
-	for(int i=0;i<16;i++){    //storing card number in an array of 16 integers
-		cin>>card_num[i];
-	}
+	cin.ignore();
+    getline(cin, card_num);
+    
+    while(card_num.length()!=15){
+    	cout<<"\n"<<"Invalid card number. Card number must contain 16 digits."<<endl;
+    	cout<<"Please enter the Card Number again: ";
+		cin.ignore();
+	    getline(cin, card_num);
+    }
+
 	cout<<"\n";
 	cout<<"Card Expiration Date: ";
 	cin>>exp_date;
 	cout<<"\n";
+
 	cout<<"Three Digit CVV Code: ";
-	cin>>cvv;
-	cout<<"\n";
-	cout<<"Enter the number of installments you wish to pay in: ";
-	cin>>installments_num;
+	cin.ignore();
+    getline(cin, cvv);  
+    if(cvv.length()!=3){
+    	cout<<"\n"<<"Invalid cvv code. Cvv code must contain 3 digits."<<endl;
+    	cout<<"Please enter your three digit cvv code again: ";
+		cin.ignore();
+	    getline(cin, cvv);
+    	while(cvv.length()!=2){
+    		cout<<"\n"<<"Invalid cvv code. Cvv code must contain 3 digits."<<endl;
+	    	cout<<"Please enter your three digit cvv code again: ";
+			cin.ignore();
+		    getline(cin, cvv);
+       }
+    }  
 	cout<<"\n";
 
-//stroing the last 4 digits of card number
-	card_end_num[0] = card_num[12];  
-	card_end_num[1] = card_num[13];
-	card_end_num[2] = card_num[14];
-	card_end_num[3] = card_num[15];
+	cout<<"Enter the number of installments you wish to pay in (2, 3, or 4): ";
+	cin>>installments_num_double;
+	installments_num = floor(installments_num_double);
+	cout<<"\n";
+
+	while(installments_num<2 || installments_num>4){
+		cout<<"\n"<<"Invalid input. You can only pay in installments of 2, 3 or 4."<<endl;
+		cout<<"Enter the number of installments you wish to pay in: ";
+	    cin>>installments_num_double;
+	    installments_num = floor(installments_num_double);
+	}
+
+//storing the last 4 digits of card number
+	card_end_num[0] = card_num[11];  
+	card_end_num[1] = card_num[12];
+	card_end_num[2] = card_num[13];
+	card_end_num[3] = card_num[14];
 }
 
 //displays payment method name and ending 4 digits of card
 void LaterPay::getPaymentInfo(){
 	cout<<"Paid with: LaterPay and Card ending in ";
  	cout<<card_end_num[0]<<card_end_num[1]<<card_end_num[2]<<card_end_num[3]<<endl;
- 	cout<<"First Installment Amount Paid: $"<<getPricePerInstallment()<<endl;
+ 	cout<<"First Installment of "<<installments_num<<" installments paid";
+ 	cout<<" with price per installment of $"<<getPricePerInstallment()<<endl;
 }
 
 int LaterPay::getInstallmentsNum(){
@@ -186,6 +256,8 @@ double LaterPay::getPricePerInstallment(){
 LaterPay::~LaterPay(){
 	cout<<"Destroyed a LaterPay object."<<endl;
 }
+
+
 
 
 
